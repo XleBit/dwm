8872a4e0fbec8712bfae3bb835578343bd8020bd
XleBit <64214021+XleBit@users.noreply.github.com>
2022-07-20 12:05:54 +0000

    Add files via upload

--- config.h
+++ config.h
@@ -14,21 +14,21 @@ static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
 #define ICONSIZE 16                             /* icon size */
 #define ICONSPACING 5                           /* space between icon and title */
-static const char *fonts[]          = { "xos4terminus:size=12:antialias=true" };
-static const char dmenufont[]       =   "xos4terminus:size=12:antialias=true" ;
-static const char col_gray1[]       = "#201A24"; /* def 222222 || 101010 || 282828 || 111011*/
+static const char *fonts[]          = { "caskaydiacovenerdfont:size=11:antialias=true:autohint=true" };
+static const char dmenufont[]       =   "monospace:size=10" ;
+static const char col_gray1[]       = "#191919"; /* def 222222 || 101010 || 282828 || 111011*/
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#888888";
 static const char col_gray4[]       = "#EEEEEE";
-static const char col_cyan[]        = "#4D3066"; /* def 005577 ; Random color selection: 432A3D 478061 500077 23a99b 00cd00 3cb371 cd6500 */
+static const char col_cyan[]        = "#FFFFFF"; /* def 005577 ; Random color selection: 432A3D 478061 500077 23a99b 00cd00 3cb371 cd6500 */
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-    [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-    [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+    [SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
+    [SchemeSel]  = { col_gray1, col_cyan,  col_cyan  },
 };
 
 /* tagging */
-static const char *tags[] = { "!", "?", ")", "#", "$", "%", "/", "&", ";" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
 static const Rule rules[] = {
     /* xprop(1):
@@ -36,12 +36,10 @@ static const Rule rules[] = {
      *	WM_NAME(STRING) = title
      */
     /* class      instance    title       tags mask     isfloating   monitor */
-    { "Gimp",     NULL,       NULL,       0,            1,           -1 },
     { "Thunar",   "thunar",   NULL,       0,            1,           -1 },
-    { "Steam",    NULL,       NULL,       0,            1,           -1 },
     { "pcsx2",    "Pcsx2",    NULL,       0,            1,           -1 },
     { "vlc",      "vlc",      NULL,       0,            1,           -1 },
-	{ "mpv",      "gl",       NULL,       0,            1,           -1 },
+    { "mpv",      "gl",       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -62,6 +60,8 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask
+#define SUPKEY Mod4Mask
+
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -73,19 +73,26 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-s",NULL };
-static const char *termcmd[]  = { "alacritty",   NULL };
-static const char *webcmd[]   = { "librewolf",   NULL };
-static const char *qutecmd[]  = { "qutebrowser", NULL };
-static const char *filecmd[]  = { "thunar",  NULL };
+static const char *dmenucmd[]  = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_cyan, "-sf", col_gray1, "-s", NULL };
+static const char *termcmd[]   = { "alacritty", NULL };
+static const char *webcmd[]    = { "librewolf", NULL };
+static const char *webaltcmd[] = { "qutebrowser", NULL };
+static const char *filecmd[]   = { "thunar", NULL };
+static const char *editorcmd[] = { "emacsclient", "-c", NULL };
+static const char *emailcmd[]  = { "thunderbird", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function        argument */
-    { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-    { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd  } },
-    { MODKEY,                       XK_w,      spawn,          {.v = webcmd   } },
-    { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = qutecmd  } },
-    { MODKEY,                       XK_e,      spawn,          {.v = filecmd  } },
+	/* commands */
+    { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd  } },
+    { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd   } },
+    { MODKEY,                       XK_w,      spawn,          {.v = webcmd    } },
+    { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = webaltcmd } },
+    { MODKEY,                       XK_e,      spawn,          {.v = filecmd   } },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = editorcmd } },
+	{ MODKEY|ShiftMask,             XK_d,      spawn,          {.v = emailcmd  } },
+
+	/* dwm functions */
     { MODKEY,                       XK_b,      togglebar,      {0} },
     { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
     { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
